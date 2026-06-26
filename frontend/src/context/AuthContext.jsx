d7cb3263d3667592e297726460431d2ee92b1bcd
Prahlad Singh <prahladsingh7512@gmail.com>
2026-06-26 23:26:37 +0530

    Update AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -107,6 +107,6 @@ useEffect(() => {
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
-  if (!context) throw new Error('useAuth must be used within AuthProvider');
+  if (!context) throw new Error('useAuth must be used within AuthProvider!');
   return context;
 };
